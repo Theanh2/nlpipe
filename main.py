7f920b6d46477c2a25e56478e98304a223322157
theanh <Theanh_v99@yahoo.de>
2021-12-06 23:03:06 +0100

    06.12.2021 tokenization Pipeline if clauses for defaults, readme file

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from IPython.display import display
-
-data = pd.read_csv('nlpPipeline/WikiQA-dev.txt', delimiter = "\t", header=None)
-data.columns = ["question", "answer", "true"]
-data.head()
-display(data)
